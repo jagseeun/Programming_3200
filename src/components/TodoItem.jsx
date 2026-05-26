9ee34ae737906e014a61f4391f248b8529c1e128
jagseeun <s2468@e-mirim.hs.kr>
2026-05-26 10:09:08 +0900

    3214 장세은  1-1	날짜 표시?	★	Date, 컴포넌트 렌더링

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -39,7 +39,6 @@ export default function TodoItem({ todo, toggleTodo, deleteTodo, editTodo }) {
                     autoFocus
                 />
             }
-            <span>날짜</span>
             <Button
                 className="todo__button todo__button--edit"
                 onClick={handleEditClick}
